295f731a884d36e778aa1c7065fe70a73e6cb0c6
ivannash23 <ivannash23@Gmail.com>
2017-04-12 04:25:17 -0700

    NCO is now able to assign task to non-NCOs

--- app/controllers/task_user_controller.rb
+++ app/controllers/task_user_controller.rb
@@ -6,6 +6,19 @@ class TaskUserController < ApplicationController
 		
 	end
 
+	def assign
+		@user = User.find_by_name(params[:name])
+		if not @user.nil?
+		@task = Task.find(params[:tasks_id])
+		@user.tasks.push(@task)
+		redirect_to task_users_index_path(@user.id)
+		else
+			flash[:notice] = "Marine not found"
+			redirect_to tasks_index_path
+
+		end
+	end
+
 	def create
     @user = current_user
     @task = Task.find(params[:tasks_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :tasks
   root to: 'users#index'
 
-  get "/tasks", to: 'tasks#index'
+  get "/tasks", to: 'tasks#index', as: "tasks_index"
   get '/users', to: 'users#index'
   get '/users/:user_id/tasks', to: 'task_user#index', as: "task_users_index"
+  post "/tasks/:tasks_id/users", to: 'task_user#assign', as: "task_users_assign"
   post "/tasks/:tasks_id/users", to: 'task_user#create', as: "task_users"
 end
